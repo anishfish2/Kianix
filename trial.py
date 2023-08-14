5a3ed8c515ccba4caba5dca1909048b3b771fdeb
anishfish2 <anishkarthik21@gmail.com>
2023-08-13 20:01:32 -0500

    Add mario

--- trial.py
+++ trial.py
@@ -1,33 +1,4 @@
-import tkinter as tk
-import win32api
-import win32con
-import pywintypes
-
-def display_text(text, display_time):
-    def close_window():
-        label.master.destroy()
-
-    label = tk.Label(text=text, font=('Segoe Script', '80'), fg='black', bg='white')
-    label.master.overrideredirect(True)
-    label.master.geometry("+250+250")
-    label.master.lift()
-    label.master.wm_attributes("-topmost", True)
-    label.master.wm_attributes("-disabled", True)
-    label.master.wm_attributes("-transparentcolor", "white")
-
-    hWindow = pywintypes.HANDLE(int(label.master.frame(), 16))
-    exStyle = win32con.WS_EX_COMPOSITED | win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT
-    win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)
-
-    label.pack()
-
-    # Close the window after the specified display time (in milliseconds)
-    label.after(display_time, close_window)
-
-    label.mainloop()
-
-def main():
-    display_text("Hello, World!", 5000)
-
-if __name__ == '__main__':
-    main()
+# Note: you need to be using OpenAI Python v0.27.0 for the code below to work
+import openai
+audio_file= open("C:/Users/Owner/Desktop/Projects/Kianix/temp.wav", "rb")
+transcript = openai.Audio.transcribe("whisper-1", audio_file)
